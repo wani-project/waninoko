d25b638309e48083bfe67c5692b0fa4c15443d1a
OVS20695@KCSF2014.onmicrosoft.com <OVS20695@KCSF2014.onmicrosoft.com>
2020-12-18 15:02:04 +0900

    1218 仮統合

--- wani1/L6.cs
+++ wani1/L6.cs
@@ -63,6 +63,28 @@ namespace wani1
                     //最前面に設定
                     wani.BringToFront();
                     break;
+                case "seikai":
+                    PictureBox seikai = new PictureBox();
+                    seikai.Name = "seikai";
+                    seikai.Size = new Size(646, 587);
+                    seikai.SizeMode = PictureBoxSizeMode.StretchImage;
+                    seikai.Image = Image.FromFile(FilePath + "\\images\\seikai.gif");
+                    seikai.Location = new Point(573, 0);
+                    seikai.Parent = panel4;
+                    panel4.Controls.Add(seikai);
+                    seikai.BringToFront();
+                    break;
+                case "miss":
+                    PictureBox miss = new PictureBox();
+                    miss.Name = "miss";
+                    miss.Size = new Size(646, 587);
+                    miss.SizeMode = PictureBoxSizeMode.StretchImage;
+                    miss.Image = Image.FromFile(FilePath + "\\images\\matigai.gif");
+                    miss.Location = new Point(573, 0);
+                    miss.Parent = panel4;
+                    panel4.Controls.Add(miss);
+                    miss.BringToFront();
+                    break;
             }
 
             
@@ -118,47 +140,107 @@ namespace wani1
             Start();
         }
 
-        private void Start()
+        private async void Start()
         {
             //分岐処理部分----------------------------------------------------------------------
             if (satoshi == 2)
             {
                 if (hanako == 1 && ans == 2)
                 {
-                    textBox1.Text = "せいかい！！";
+                    textBox1.Text = "せいかい！！すごい！！！";
+                    CreateControl("seikai", 2);
+                    await Task.Delay(2825);
+                    Control[] re = new Control[0];
+                    re = panel4.Controls.Find("seikai", true);
+                    foreach(Control c in re)
+                    {
+                        panel4.Controls.Remove(c);
+                    }
+
+                    //========================================
+                    waniTalk();
+                    //========================================
                 }
                 else
                 {
-                    textBox1.Text = "まちがい";
+                    textBox1.Text = "まちがい...つぎはできるよ！\r\nがんばって！！！";
+                    CreateControl("miss", 2);
+                    await Task.Delay(2800);
+                    Control[] re = new Control[0];
+                    re = panel4.Controls.Find("miss", true);
+                    foreach (Control c in re)
+                    {
+                        panel4.Controls.Remove(c);
+                    }
+
+                    //========================================
+                    waniTalk();
+
+                    //========================================
+
                 }
             }
             else if(hanako == 1)
             {
-                textBox1.Text = "まちがい";
+                textBox1.Text = "まちがい...つぎはできるよ！\r\nがんばって！！！";
+                CreateControl("miss", 2);
+                await Task.Delay(2800);
+                Control[] re = new Control[0];
+                re = panel4.Controls.Find("miss", true);
+                foreach (Control c in re)
+                {
+                    panel4.Controls.Remove(c);
+                }
+
+                //========================================
+                waniTalk();
+
+                //========================================
             }
             else
             {
-                textBox1.Text = "まちがい";
+                textBox1.Text = "まちがい...つぎはできるよ！\r\nがんばって！！！";
+                CreateControl("miss", 2);
+                await Task.Delay(2800);
+                Control[] re = new Control[0];
+                re = panel4.Controls.Find("miss", true);
+                foreach (Control c in re)
+                {
+                    panel4.Controls.Remove(c);
+                }
+
+                //========================================
+                waniTalk();
+
+                //========================================
             }
         }
         
         //---------------------------------------------------------------------
+        private async void waniTalk()
+        {
+            //========================================
+            talkflg = true;
+            CreateControl("waniChara", 0);
+            await Task.Delay(5000);
+            talkflg = false;
+            CreateControl("waniChara", 0);
 
+            //========================================
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //さとしくん
+            //せいかい
             ans = 1;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            //はなこさん
+            //はずれ
             ans = 2;
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
 
         }
     }
@@ -169,5 +251,4 @@ namespace wani1
 
 
 
-       
-
+    
