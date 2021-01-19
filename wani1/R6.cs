f7a4039d68918374673fe569b0ef753bf80d6baa
りょ <s3a2@KCSF0602>
2021-01-19 12:17:25 +0900

    0119

--- wani1/R6.cs
+++ wani1/R6.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace wani1
 {
     public partial class R6 : Form
     {
+        private string FilePath = Directory.GetCurrentDirectory();
         public R6()
         {
             InitializeComponent();
@@ -34,7 +36,7 @@ namespace wani1
 
         private void R6_Load(object sender, EventArgs e)
         {
-            
+            CreateNum();
         }
         //移動処理の初期変数---------
         bool _isDraging = false;
@@ -71,7 +73,7 @@ namespace wani1
             int w = width / 5;
             for (int x = w; x <= width; x += w)
             {
-                int bufX = 0;
+                int bufX = w;
                 if(bufX < point.X && point.X < x)
                 {
                     countX++;
@@ -80,7 +82,7 @@ namespace wani1
                     countY = 0;
                     for(int y = h; y <= height; y += h)
                     {
-                        int bufY = 0;
+                        int bufY = h;
                         if(bufY < point.Y && point.Y < y)
                         {
                             countY++;
@@ -90,6 +92,29 @@ namespace wani1
                 }
             }
             MessageBox.Show(countX.ToString() + ", " + countY.ToString());
+
+        }
+        private void CreateNum()
+        {
+            PictureBox zero = new PictureBox();
+            PictureBox one = new PictureBox();
+            PictureBox two = new PictureBox();
+
+            zero.Size = new Size(100, 100);
+            zero.SizeMode = PictureBoxSizeMode.StretchImage;
+            zero.Image = Image.FromFile(FilePath + "\\images\\C2\\0.png");
+
+            one.Size = new Size(100, 100);
+            one.SizeMode = PictureBoxSizeMode.StretchImage;
+            one.Image = Image.FromFile(FilePath + "\\images\\C2\\1.png");
+
+            two.Size = new Size(100, 100);
+            two.SizeMode = PictureBoxSizeMode.StretchImage;
+            two.Image = Image.FromFile(FilePath + "\\images\\C2\\2.png");
+
+            tableLayoutPanel1.Controls.Add(zero,0,0);
+            tableLayoutPanel1.Controls.Add(one,1,1);
+            tableLayoutPanel1.Controls.Add(two,2,2);
         }
     }
 }
